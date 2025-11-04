4a8eed601221b21e06c622588eb379c56961973a
Mike Samimi <msamimi@Mikes-MacBook-Pro.local>
2025-11-03 19:05:03 -0500

    Remove dark theme toggle - enforce light mode for all users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Admin, Resource } from 'react-admin';
 import { dataProvider } from './dataProvider';
 import { authProvider } from './authProvider';
 import { AuthProvider, useAuth } from './context/AuthContext';
-import { customLightTheme, customDarkTheme } from './theme';
+import { customLightTheme } from './theme';
 
 // NOTE: React Admin provides its own router - do NOT wrap in BrowserRouter!
 
@@ -41,7 +41,6 @@ const AdminApp = () => {
       title="PM Command Center"
       layout={CustomLayout}
       theme={customLightTheme}
-      darkTheme={customDarkTheme}
       loginPage={Login}
       requireAuth
       disableTelemetry
